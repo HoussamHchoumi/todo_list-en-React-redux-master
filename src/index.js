bea053bf4192a4ce6e0bd79ec0761408d056a453
HOUSSAM EDDINE <houssamhchoumi@gmail.com>
2023-02-04 15:13:45 +0100

    change commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App.js";
 import store from "./store.js";
 import { Provider } from "react-redux";
